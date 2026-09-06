8e66270514ae65f3f4a1905dbe863b72e7f673f5
yyfx <118585028+yydfys@users.noreply.github.com>
2026-09-06 20:18:51 +0800

    检查更新仓库指向改为 yydfys/webhtv-bd

--- app/src/main/java/com/fongmi/android/tv/utils/Github.java
+++ app/src/main/java/com/fongmi/android/tv/utils/Github.java
@@ -2,13 +2,13 @@ package com.fongmi.android.tv.utils;
 
 public class Github {
 
-    private static final String GITHUB_LATEST = "https://github.com/Silent1566/webhtv/releases/latest/download";
-    private static final String GITHUB_RELEASE = "https://github.com/Silent1566/webhtv/releases/download";
+    private static final String GITHUB_LATEST = "https://github.com/yydfys/webhtv-bd/releases/latest/download";
+    private static final String GITHUB_RELEASE = "https://github.com/yydfys/webhtv-bd/releases/download";
     private static final String GITHUB_UPDATE_CHANNEL = GITHUB_RELEASE + "/update-channel";
     private static final String CNB_MANIFEST = "https://cnb.cool/fish2035/webhtv-release/-/git/raw/main/apk";
-    private static final String GITHUB_API = "https://api.github.com/repos/Silent1566/webhtv/releases/tags";
-    private static final String GITHUB_RELEASES_API = "https://api.github.com/repos/Silent1566/webhtv/releases";
-    private static final String GITHUB_RELEASE_ASSETS_API = "https://api.github.com/repos/Silent1566/webhtv/releases/assets";
+    private static final String GITHUB_API = "https://api.github.com/repos/yydfys/webhtv-bd/releases/tags";
+    private static final String GITHUB_RELEASES_API = "https://api.github.com/repos/yydfys/webhtv-bd/releases";
+    private static final String GITHUB_RELEASE_ASSETS_API = "https://api.github.com/repos/yydfys/webhtv-bd/releases/assets";
 
     public static String getChannelAsset(String name) {
         return GITHUB_UPDATE_CHANNEL + "/" + name;
